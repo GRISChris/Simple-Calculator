803d86d5bc7a73b866fe97e63ef93ece00ecc1ae
okdad <axiz.bali@gmail.com>
2018-07-16 16:28:17 +0700

    the program
    h

---
+++ SimpleCalculator.py
@@ -0,0 +1,71 @@
+print("Calculator")
+
+FirstNum = False
+SecondNum = False
+RetryNum = False
+RestartQuestion = False
+
+def retry(Num1, RestartQuestion):
+	retry = input("are you finished calculating this number? [Y/N]\n")
+	retry = retry.lower()
+	while RestartQuestion == False:
+		if retry in ("y", "yes"):
+			quit()
+		elif retry in ("n", "no"):
+			CalcReset(Num1, False)
+		else:
+			print("no\n")
+
+def Calculation(Num1, Num2):
+	Ask = input("What do you want to do with it? [+,-,*,/,%]\n")
+	if Ask in ("+", "-", "*", "/", "%"):
+		if Ask == "+":
+			Num1 = Num1 + Num2
+			print("The Result is %f" % Num1)
+			retry(Num1, False)
+		elif Ask == "-":
+			Num1 = Num1 - Num2
+			print("The Result is %f" % Num1)
+			retry(Num1, False)
+		elif Ask == "*":
+			Num1 = Num1 * Num2
+			print("The Result is %f" % Num1)
+			retry(Num1, False)	
+		elif Ask == "/":
+			Num1 = Num1/Num2
+			print("The Result is %f" % Num1)
+			retry(Num1, False)
+		elif Ask == "%":
+			Num1 = Num1%Num2
+			print("The Result is %f" % Num1)
+			retry(Num1, False)
+	else:
+		print("what")
+		Calculation(Num1, Num2)
+				
+
+
+def CalcReset(Num1, RetryNum):
+	print("Okay, your current number from last result is %d " % Num1)
+	Num2 = float(input("Please input number 2\n"))
+	while RetryNum == False:
+			try:
+				RetryNum == True
+				Calculation(Num1, Num2)
+			except ValueError:
+				print("no")
+
+
+while FirstNum == False:
+	Num1 = float(input("pls input number 1\n"))
+	try:
+		FirstNum == True
+		while SecondNum == False:
+			Num2 = float(input("pls input number 2\n"))
+			try:
+				SecondNum == True
+				Calculation(Num1, Num2)
+			except ValueError:
+				print ("can u not")
+	except ValueError:
+		print("no\n")
